c9a9dffa1ba4b8a7a47910d2da3807f0b5bd76cd
Rob Dickinson <robfromboulder@gmail.com>
2017-08-15 23:12:48 -0600

    Add post handler

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,19 @@ import java.util.Map;
 
 import static spark.Spark.*;
 
+import io.resurface.HttpLogger;
+
 public class Main {
 
     public static void main(String[] args) {
 
         port(Integer.valueOf(System.getenv("PORT")));
         staticFileLocation("/public");
+
+        HttpLogger logger = new HttpLogger("$LOGGER_URL");
+        after((request, response) -> {
+            if (response.body() != null) logger.log(request.raw(), request.body(), response.raw(), response.body());
+        });
 
         get("/hello", (req, res) -> "Hello World");
 
@@ -20,6 +27,8 @@ public class Main {
             attributes.put("message", "Hello World!");
             return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());
+
+        post("/", (req, res) -> "Received POST message: " + req.body());
 
     }
 
